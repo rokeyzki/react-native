c79b02fd0e8c0dc3cf0d67d447520548a0650ad6
林川淇 <rokeyzki@gmail.com>
2017-08-08 22:18:47 +0800

    textinput

--- App.js
+++ App.js
@@ -4,16 +4,16 @@ import React from 'react';
 // import AntdMobileExam from './src/components/antd-mobile';
 // import SimpleApp from './src/components/navigation';
 // import Flexbox from './src/components/flexbox/example-first';
-import LeanCloudExam from './src/components/leancloud';
+// import LeanCloudExam from './src/components/leancloud';
 
-// import AppRoot from './src/components/playground/demo-first';
+import AppRoot from './src/components/playground/demo-first';
 
 export default class App extends React.Component {
   render() {
     console.ignoredYellowBox = ['Remote debugger']; // Chrome远程调试时启用这行代码
 
     return (
-      <LeanCloudExam />
+      <AppRoot />
     );
   }
 }
--- src/components/playground/demo-first/home-screen.js
+++ src/components/playground/demo-first/home-screen.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native';
+import { Text, View, Button, TextInput } from 'react-native';
 
 export default class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      inputValue: '',
+    }
+
+    this.getInputValue = this.getInputValue.bind(this);
+  }
+
   static navigationOptions = {
     title: 'Welcome',
-  };
+  }
+
+  getInputValue() {
+    // console.dir(this.refs.myInput);
+
+    this.setState({
+      inputValue: this.refs.myInput._lastNativeText
+    })
+  }
 
   render() {
     const { navigate } = this.props.navigation;
@@ -15,6 +33,15 @@ export default class HomeScreen extends React.Component {
           onPress={() => navigate('Other')}
           title="Other Button"
         />
+        <TextInput
+          ref="myInput"
+          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+        />
+        <Button
+          onPress={this.getInputValue}
+          title="Get Input Value"
+        />
+        <Text>{`value: ${this.state.inputValue}`}</Text>
       </View>
     );
   }
